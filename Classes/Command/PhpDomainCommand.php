fe91bb648ee478f19e7c0f19b2b024f0393ea6fc
lina.wolf <lwolf@w-commerce.de>
2022-02-20 13:24:37 +0100

    first commit

---
+++ Classes/Command/PhpDomainCommand.php
@@ -0,0 +1,35 @@
+<?php
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+
+class PhpDomainCommand extends Command
+{
+    /**
+     * Configure the command by defining the name, options and arguments
+     */
+    protected function configure()
+    {
+        $this->setHelp('Prints a list of recent sys_log entries.' . LF . 'If you want to get more detailed information, use the --verbose option.');
+    }
+
+    /**
+     * Executes the command for showing sys_log entries
+     *
+     * @param InputInterface $input
+     * @param OutputInterface $output
+     * @return int error code
+     */
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $io = new SymfonyStyle($input, $output);
+        $io->title($this->getDescription());
+
+        // ...
+        $io->writeln('Write something');
+        return Command::SUCCESS;
+    }
+}
